21e25e7ff1efd033bd158e909b4de115b2ba1b2e
hazem ibrahim <hazibra2001@gmaill.com>
2023-03-05 16:28:31 +0200

    custom user model

--- school_guideline/urls.py
+++ school_guideline/urls.py
@@ -6,3 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+
+
+admin.site.site_header = "School Guideline Admin"
+admin.site.site_title = "School Guideline Admin Portal"
+admin.site.index_title = "Welcome to the School Guideline Portal"
